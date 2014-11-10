b3acf7ca0407eb7bb23aa833346a8d456504a667
afifmucholis <afif.mucholis@gmail.com>
2014-11-10 17:08:55 +0700

    tamabah penitip

---
+++ tambah.php
@@ -0,0 +1,8 @@
+<form method="POST" action="hasil.php" enctype="multipart/form-data">
+	Id pengguna <input type="number" name="id"> <br>
+	Nama Pengguna <input type="text" name="nama"> <br>
+	No HP  <input type="text" name="hp"> <br>
+	Deskripsi barang <input type="text" name="deskripsi"> <br>
+	Nomor Loker <input type="number" name="loker"> <br>
+	<input type="submit" name="tambah" value="Tambah"> <br>
+</form>
