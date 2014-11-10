5847a5bdfd12add3f18889a8745657d08df927ba
febri5767 <febri.luh.r@mail.ugm.ac.id>
2014-11-10 16:34:26 +0700

    tambah pengguna

---
+++ Penitipan/tabel_pengguna.php
@@ -0,0 +1,62 @@
+<html>
+<head>
+	<title>tabel pengguna</title>
+	</head>
+	<body>
+	<?php
+		$host='localhost';
+		$dbname='penitipan';
+		$user='root';
+		$pass='';
+		
+		$con = mysqli_connect($host, $user, $pass, $dbname);
+		
+		if(mysqli_connect_errno()){
+			echo "koneksi error: ". mysqli_connect_error();
+		
+		}else{
+		
+		echo "";
+		}
+		
+		//membuat koneksi database dengan php
+		$query = mysqli_query($con, "select * from pengguna");
+		//mysqli_fetch_row();   menampilkan database yang akan di import
+		//mysqli_fetch_array();	menampilkan database yang akan di import
+		
+		echo "<h2> Daftar pengguna</h2>";
+		?>
+			<a href="tambah.php">tambah data pengguna</a>
+			<table>
+			<tr><th>ID</th>
+			<th>Nama</th>
+			<th>No. HP</th>
+			<th>Deskripsi</th>
+			<th>No. Loker</th>
+			<th>Waktu</th>
+			<th colspan="2"> aksi</th>
+			</tr>
+			
+		<?php
+		$i=1;
+		while ($row = mysqli_fetch_array($query)){
+		
+		echo "<tr>";
+		echo "<td>".$i."</td>";
+		echo "<td>".$row['nama']."</td>";
+		echo "<td>".$row['no_hp']."</td>";
+		echo "<td>".$row['deskripsi']."</td>";
+		echo "<td>".$row['no_loker']."</td>";
+		echo "<td>".$row['waktu']."</td>";
+		
+		echo "<td><a href=\"edit2.php?&id=".$row['id']."\">edit</a></td>";
+		echo "<td><a href=\"hapus2.php?&id=".$row['id']."\"onclick=\"alert('anda akan menghapus barang!')\">hapus</a></td>";
+		echo "</tr>";
+		$i++;
+		}
+	
+	
+
+	
+	?>
+	</table>
