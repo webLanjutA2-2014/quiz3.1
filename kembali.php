3e5d1b8b7faacfd7838be7588a7d83b5caf414a0
$ RVM -V <afif.mucholis@gmail.com>
2014-11-11 10:03:01 +0700

    proses untuk mengembalikan

--- kembali.php
+++ kembali.php
@@ -1,8 +1,29 @@
 
 <?php
 	require("koneksi.php");
-	$waktu = now();
-	echo $waktu;
-		
+session_start();
+if(isset($_SESSION['login']) && ($_SESSION['login'] == 1)){
+	
+	$id = $_GET['id'];
+	$sql = "UPDATE `penitipan`.`pengguna` SET `waktu_selesai` = now(), `diambil` = '1' WHERE `pengguna`.`id` =". $id."";
+	mysqli_query($con, $sql) or die('error PRET!!!!'. mysqli_error($con));
 
+	function tarif ($id, $con){
+
+		$q = "SELECT TIMESTAMPDIFF(MINUTE, waktu_mulai, waktu_selesai) as lama FROM pengguna WHERE id = $id";
+		$s = mysqli_query($con, $q);
+		$a = mysqli_fetch_array($s);
+		$tarif = $a['lama'] * (1000/60);
+		$up = mysqli_query($con, "UPDATE `penitipan`.`pengguna` SET tarif = $tarif WHERE `pengguna`.`id` =". $id."");
+	}
+
+	tarif($id, $con);
+
+	
+	
+	header('location:detail.php?&id='.$id.'');
+	}else{
+	header('location: login.php');
+}
+?>
 ?>
