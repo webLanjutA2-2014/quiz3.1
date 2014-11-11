3d5290dcae5d44ed36cdfc8021b8995901822f22
$ RVM -V <afif.mucholis@gmail.com>
2014-11-11 10:03:22 +0700

    halaman home petugas

--- index.php
+++ index.php
@@ -2,14 +2,17 @@
 include "koneksi.php";
 session_start();
 if(isset($_SESSION['login']) && ($_SESSION['login'] == 1)){
+
 	echo "<h2> Selama datang ".$_SESSION['name']. "</h2>";
 	?>
-
+	<ul>
 	<a href="tambah.php"> Tambah Pengguna </a> <br>
+	<a href="lihat.php"> Lihat Daftar Pengguna </a> <br>
+	</ul>
 
 <?php
 }else{
 	header('location: login.php');
 }
 ?>
-<a href ="logout.php"> Locot </a>
+<a href ="logout.php"> Logout </a>
