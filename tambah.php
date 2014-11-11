76bced415873bf4332f2e89ec35230e71c40bc29
$ RVM -V <afif.mucholis@gmail.com>
2014-11-11 10:02:03 +0700

    tambah pengguna

--- tambah.php
+++ tambah.php
@@ -1,8 +1,35 @@
+<?php 
+include "koneksi.php";
+session_start();
+if(isset($_SESSION['login']) && ($_SESSION['login'] == 1)){
+	?>
+	<a href="lihat.php"> Daftar Pengguna </a>| <a href="logout.php"> Logout </a> <br>
+	<strong>Tambah Pengguna</strong>
 <form method="POST" action="hasil.php" enctype="multipart/form-data">
-	Id pengguna <input type="number" name="id"> <br>
-	Nama Pengguna <input type="text" name="nama"> <br>
-	No HP  <input type="text" name="hp"> <br>
-	Deskripsi barang <input type="text" name="deskripsi"> <br>
-	Nomor Loker <input type="number" name="loker"> <br>
-	<input type="submit" name="tambah" value="Tambah"> <br>
+	<table>
+		<tr>
+			<td>Id pengguna </td> <td> <input type="number" name="id"> </td>
+		</tr>
+		<tr>
+			<td>Nama Pengguna </td> <td>  <input type="text" name="nama"> </td>
+		</tr>
+	 	<tr>
+			<td>No HP </td> <td>  <input type="text" name="hp"> </td>
+		</tr>
+		<tr>
+			<td>Deskripsi Barang </td> <td>  <textarea name="deskripsi"> </textarea> </td>
+		</tr>
+		<tr>
+			<td>Nomor Loker </td> <td>  <input type="number" name="loker"> </td>
+		</tr>
+	  	<tr>
+	  		<td colspan="2"> <input type="submit" name="tambah" value="Tambah"> </td>
+	  	</tr>
+	
+	</table>
 </form>
+<?php
+}else{
+	header('location: login.php');
+}
+?>
